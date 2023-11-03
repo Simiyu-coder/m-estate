50239c82f915a7ff318498a6487cb003d25aa068
Simiyu-coder <simiyukelvin93@gmail.com>
2023-11-03 17:14:37 +0300

    created pages and routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className='bg-slate-200 md-shadow p-3 '>
+        <span className='text-slate-500'>M-</span>
+        <span className='text-slate-700'>Estate</span>
+      </h1>
+    </header>
+  )
+}
