89aa8316cafa884030c352161722b030453526ff
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-11-17 09:59:09 +0300

    fixed bugs

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use((cors({
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!')
+    console.log('Server is running !!')
 })
 
 app.use('/api/user', userRouter);
